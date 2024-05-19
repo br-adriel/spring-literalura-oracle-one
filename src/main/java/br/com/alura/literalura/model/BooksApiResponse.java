6a4e82577643e7c0282fe5beda9912af959b3e99
Adriel <adriel.fsantos@outlook.com>
2024-05-18 21:43:23 -0300

    feat: Add BooksApiResponse record

---
+++ src/main/java/br/com/alura/literalura/model/BooksApiResponse.java
@@ -0,0 +1,15 @@
+package br.com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record BooksApiResponse(
+        @JsonAlias("count") Integer quantidade,
+        @JsonAlias("previous") String linkPaginaAnterior,
+        @JsonAlias("next") String linkPaginaPosterior,
+        @JsonAlias("results") List<BookData> livros
+) {
+}
