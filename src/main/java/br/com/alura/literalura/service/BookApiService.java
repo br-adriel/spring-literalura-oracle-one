3463c7cb5dcb598bfa71b7eae861c5ddd1b29bfe
Adriel <adriel.fsantos@outlook.com>
2024-05-19 20:03:03 -0300

    feat: Add getAuthorsAliveOnYear method

--- src/main/java/br/com/alura/literalura/service/BookApiService.java
+++ src/main/java/br/com/alura/literalura/service/BookApiService.java
@@ -2,6 +2,7 @@ package br.com.alura.literalura.service;
 
 import br.com.alura.literalura.model.BookData;
 import br.com.alura.literalura.model.BooksApiResponse;
+import br.com.alura.literalura.model.PersonData;
 import org.springframework.stereotype.Service;
 
 import java.net.URLEncoder;
@@ -10,11 +11,25 @@ import java.util.List;
 
 @Service
 public class BookApiService {
+    private final String baseUrl = "https://gutendex.com/books/";
+
     public List<BookData> search(String text) {
         var json = ApiConsumer.getData(
-                "https://gutendex.com/books/?search="
+                baseUrl + "?search="
                 + URLEncoder.encode(text.trim().toLowerCase(), StandardCharsets.UTF_8)
         );
         return new DataConverter().convert(json, BooksApiResponse.class).livros();
+    }
+
+    public List<PersonData> getAuthorsAliveOnYear(Integer year) {
+        var endpoint = baseUrl + "?author_year_start=" + year
+                       + "&author_year_end=" + year;
+        var json = ApiConsumer.getData(endpoint);
+        return new DataConverter()
+                .convert(json, BooksApiResponse.class)
+                .livros()
+                .stream()
+                .flatMap(l -> l.autores().stream())
+                .toList();
     }
 }
