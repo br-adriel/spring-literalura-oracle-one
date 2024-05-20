9c80164a7b0055951d1f0ab05a01d22c38927007
Adriel <adriel.fsantos@outlook.com>
2024-05-19 23:18:19 -0300

    feat: Add Livro class

---
+++ src/main/java/br/com/alura/literalura/model/Livro.java
@@ -0,0 +1,61 @@
+package br.com.alura.literalura.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String idioma;
+    private Integer downloads;
+
+    @ManyToOne
+    private Autor autor;
+
+    public Livro() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    public Integer getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(Integer downloads) {
+        this.downloads = downloads;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+}
